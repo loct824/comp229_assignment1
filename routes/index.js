09030519aba771cac43dc41a71704c24db960a52
loct <loct824@gmail.com>
2023-01-28 23:50:23 -0500

    added routing for required pages

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET About me page. */
+router.get('/aboutme', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+  });
+
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+  });
+  
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+  });  
+
+/* GET Contact page. */
+router.get('/contact', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+  });
+
+
 module.exports = router;
